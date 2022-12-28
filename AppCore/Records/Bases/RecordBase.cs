5d7ad351c03e4c8ecdcd779ce9263dc5b3223a74
ali.arslan <ali.arslan@ANK5-YZLMORT-10>
2022-12-28 14:18:41 +0300

    Add project files.

---
+++ AppCore/Records/Bases/RecordBase.cs
@@ -0,0 +1,9 @@
+﻿namespace AppCore.Records.Bases
+{
+    public abstract class RecordBase
+    {
+        public int Id { get; set; } // public int? Id { get; set; } = null; (int i null kullanabilmek için)
+        public string Guid { get; set; } 
+
+    }
+}
